48ed9d7c6b178bb658e7ebb2a22209337e7be9cd
Alfredo Soto <97alfredosv@gmail.com>
2018-02-16 11:11:40 -0600

    Remove JQuery like syntax for stack
    Choosing a simple way of access functions
    $(s).push(value) changes to
    stack_type_push(s, value)

--- src/list.h
+++ src/list.h
@@ -1,80 +1,89 @@
-typedef struct list { 
-	int item; 
-	struct list* next; 
-	struct list_functions* functions; 
-} list; 
-
-list* list_init(void); 
-list** Newlist(void); 
-void list_push(int pos, int item); 
-int list_pop(int); 
-bool list_isEmpty(void); 
-extern inline void list_setSelf(list** selected_LIST); 
-
-struct list_functions
-{ 
-	void (*setSelf)(list **); 
-	void (*push)(int, int);
-	int (*pop)(int);
-	bool (*isEmpty)(void);
-} static functions = { 
-	list_setSelf, 
-	list_push, 
-	list_pop, 
-	list_isEmpty 
-}; 
-
-static list** self = NULL; 
-
-extern inline void list_setSelf(list** selected_LIST) { 
-	self = selected_LIST; 
-} 
-
-list* list_init() { 
-	list* LIST = malloc(sizeof(list)); 
-	LIST->next = NULL; 
-	LIST->functions = &functions; 
-	return LIST; 
-} 
-
-list** Newlist () {
-	list* LIST = list_init(); 
-	list** obj = malloc(sizeof(list**)); 
-	obj = &LIST; 
-	return obj; 
-} 
-
-void list_push(int pos, int item) {
-	int current_pos = 0;
-	list* aux = &(**self);
-	while(aux->next && current_pos < pos) {
-		aux++;
-		pos++;
-	}
-	aux
-	 
+#define LIST(NAME, TYPE) GENERATE_DYNAMIC_LIST(NAME, TYPE)
+#define GENERATE_DYNAMIC_LIST(LIST_NAME, TYPE) typedef struct LIST_NAME { \
+	TYPE item; \
+	struct LIST_NAME* next; \
+} LIST_NAME; \
+\
+LIST_NAME* LIST_NAME##_init(void); \
+void LIST_NAME##_push(LIST_NAME** self, TYPE index, TYPE item); \
+TYPE LIST_NAME##_get(LIST_NAME** self, TYPE index); \
+void LIST_NAME##_append(LIST_NAME **self, TYPE item);\
+\
+LIST_NAME* LIST_NAME##_init() { \
+	LIST_NAME* new_LIST_NAME = malloc(sizeof(LIST_NAME)); \
+	new_LIST_NAME->next = NULL; \
+	return new_LIST_NAME; \
+} \
+\
+void LIST_NAME##_push(LIST_NAME** self, TYPE index, TYPE item) {\
+	LIST_NAME *next = LIST_NAME##_init();\
+	LIST_NAME *aux = *self;\
+	TYPE i = 0;\
+	while(i < index, aux->next != NULL) {\
+		aux = aux->next;\
+	}\
+	aux->next = next; \
+	aux->item = item;\
+}\
+\
+void LIST_NAME##_append(LIST_NAME **self, TYPE item) {\
+	LIST_NAME *next = LIST_NAME##_init();\
+	LIST_NAME *aux = *self;\
+	while(aux->next != NULL) {\
+		aux = aux->next;\
+	}\
+	aux->next = next;\
+	aux->item = item;\
+}\
+\
+TYPE LIST_NAME##_get(LIST_NAME** self, TYPE index) {\
+	LIST_NAME* aux_ptr = *self;\
+	TYPE i = 0;\
+	while(i < index && aux_ptr->next != NULL) {\
+		i++;\
+		aux_ptr = aux_ptr->next;\
+	}\
+	TYPE item = aux_ptr->item;  \
+	return item; \
+}\
+LIST_NAME *LIST_NAME##_get_ptr(LIST_NAME** self, TYPE index) {\
+	LIST_NAME* aux_ptr = *self;\
+	TYPE i = 0;\
+	while(i < index && aux_ptr->next != NULL) {\
+		i++;\
+		aux_ptr = aux_ptr->next;\
+	}\
+	return aux_ptr; \
+}\
+\
+LIST_NAME* LIST_NAME##_remove(LIST_NAME** self, TYPE index) {\
+	LIST_NAME* node = *self;\
+	LIST_NAME* previous_node;\
+	TYPE i = 0;\
+	while(i < index && node->next != NULL) {\
+		i++;\
+		previous_node = node;\
+		node = node->next;\
+	}\
+	if (node == *self) {\
+		LIST_NAME* node_to_remove = *self;\
+		*self = (**self).next;\
+		return node_to_remove;\
+	}\
+	LIST_NAME* node_to_remove = node;\
+	previous_node->next = node;\
+\
+	return node_to_remove;\
+}\
+\
+TYPE LIST_NAME##_size(LIST_NAME** self) {\
+	LIST_NAME* aux_ptr = *self;\
+	TYPE size = 0;\
+	while(aux_ptr->next != NULL) {\
+		size++;\
+		aux_ptr = aux_ptr->next;\
+	}\
+	return size; \
 }
-void list_append(int item) {
-	list *next = list_init();
-	(**self).next = next; 
-	(**self).item = item;
-	 
-}
-bool list_isEmpty() {
-	return !((**self).next); 
-}
 
-int list_pop(int index) {
-	int item = (**self).item; 
-	list* actual = *self; 
-	*self = (**self).next; 
-	return item; 
-}
-
-int list_items() {
-
-	int item = (**self).item; 
-	list* actual = *self; 
-	*self = (**self).next; 
-	return item; 
-}
+#define foreach_list(X, LIST) for(X = LIST; X->next != NULL; X = X->next)
